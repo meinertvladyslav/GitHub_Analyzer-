91bbae5d8edd9487d2821cb64ac8fd0a3befff7a
Vladyslav Meinert - STUDENT <g00462913@atu.ie>
2026-03-28 21:16:06 +0000

    Some work with git things and token

--- src/GitHub/Analyzer.java
+++ src/GitHub/Analyzer.java
@@ -1,9 +1,21 @@
 package GitHub;
 
+import java.util.Scanner;
+
 public class Analyzer {
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
+		Scanner scan = new Scanner(System.in);	
+		String url = "";
+		String auth_token = System.getenv("token");
+		
+		System.out.println("hello");
+		
+		System.out.println(auth_token);
+		if (auth_token == null || auth_token.isBlank()) {
+            throw new IllegalStateException("Missing GitHub token");
+        }
 		
 	}
 }
